ab19f342451010e8c125b0fd7f539966bd4d89b5
oussamaaatifi1 <nisowpro@gmail.com>
2024-01-25 15:21:36 +0100

    create function CreateTask

---
+++ project.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Task
+{
+    char description[1];
+    struct Date
+    {
+        int year;
+        int month;
+        int day;
+    };
+    int priority;
+};
+
+
+void CreateTask(struct Task task){
+    int *size;
+    int i;
+
+    for (i = 0; i <= *size; i++){
+        printf("Description: \n");
+        scanf("%s",&task.description);
+        printf("Due Date: dd-mm-year \n");
+        scanf("%s",&task.year, &task.month, &task.day);
+        printf("Priority: \n");
+        scanf("%s",&task.description);
+    }
+}
+void DisplayTask(struct Task task) {
+    int *size;
+    printf("\nTask Details:\n");
+    for(i = 0;i<= *size;i++){
+        printf("Description: %s\n", task.description);
+    printf("Due Date: %02d-%02d-%04d\n", task.day, task.month, task.year);
+    printf("Priority: %d\n", task.priority);
+    }
+    
+}
+
+int main() {
+    struct Task task;
+    int size;
+    printf("Enter details for the task:\n");
+    CreateTask(Task,&size);
+    DisplayTask(Task,&size);
+    // Printing the created task for demonstration purposes
+
+
+    return 0;
+}
